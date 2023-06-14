5c96f209e846f935ff39832247506fb62607d708
Ivan Draganov <draganov.ivan.an@gmail.com>
2023-06-14 19:06:20 +0200

    new login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,14 @@
-import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
+import {
+  HttpException,
+  UnauthorizedException,
+  HttpStatus,
+  Injectable,
+} from "@nestjs/common";
 import { UsersService } from "../users/users.service";
 import { JwtService } from "@nestjs/jwt";
 import * as bcrypt from "bcrypt";
 import { CreateUserDto } from "src/users/dto/create-user.dto";
+import { LoginUserDto } from "src/users/dto/login-user.dto";
 
 @Injectable()
 export class AuthService {
@@ -11,45 +17,45 @@ export class AuthService {
     private jwtService: JwtService
   ) {}
 
-  async validateUser(username: string, pwd: string): Promise<any> {
-    const user = await this.usersService.findOne(username);
-    const pwdMatch: boolean =
-      user && (await this.verifyPassword(pwd, user.password));
+  // async validateUser(username: string, pwd: string): Promise<any> {
+  //   const user = await this.usersService.findOne(username);
+  //   const pwdMatch: boolean =
+  //     user && (await this.verifyPassword(pwd, user.password));
 
-    if (pwdMatch) {
-      const { password, ...result } = user;
+  //   if (pwdMatch) {
+  //     const { password, ...result } = user;
 
-      return result;
+  //     return result;
+  //   }
+  //   return null;
+  // }
+
+  // async login(user: any) {
+  //   return {
+  //     access_token: this.jwtService.sign(user._doc),
+  //   };
+  // }
+
+  async login(dto: LoginUserDto) {
+    const user = await this.validateUser(dto);
+    return this.generateToken(user);
+  }
+  private async validateUser(dto: LoginUserDto) {
+    const user = await this.usersService.findOne(dto.username);
+    if (!user) {
+      throw new UnauthorizedException({
+        message: "Incorrect password or email",
+      });
     }
-    return null;
+    const passwordEquals = await bcrypt.compare(dto.password, user.password);
+
+    if (user && passwordEquals) {
+      return user;
+    }
+    throw new UnauthorizedException({
+      message: "Incorrect password or email",
+    });
   }
-
-  async login(user: any) {
-    return {
-      access_token: this.jwtService.sign(user._doc),
-    };
-  }
-
-  // async login(dto: LoginUserDto) {
-  //   const user = await this.validateUser(dto);
-  //   return this.generateToken(user);
-  // }
-  // private async validateUser(dto: LoginUserDto) {
-  //   const user = await this.usersService.getUserByEmail(dto.email);
-  //   if (!user) {
-  //     throw new UnauthorizedException({
-  //       message: 'Incorrect password or email',
-  //     });
-  //   }
-  //   const passwordEquals = await bcrypt.compare(dto.password, user.password);
-
-  //   if (user && passwordEquals) {
-  //     return user;
-  //   }
-  //   throw new UnauthorizedException({
-  //     message: 'Incorrect password or email',
-  //   });
-  // }
 
   async registration(dto: CreateUserDto) {
     const regUser = await this.usersService.findOne(dto.username);
---
+++ src/users/dto/login-user.dto.ts
@@ -0,0 +1,20 @@
+import {
+  IsDefined,
+  IsNotEmpty,
+  IsString,
+  Length,
+  IsEmail,
+} from "class-validator";
+
+export class LoginUserDto {
+  @IsDefined()
+  @IsString({ message: "" })
+  @IsEmail({}, { message: "incorrect username" })
+  readonly username: string;
+
+  @Length(3, 60, { message: "must be from 3 to 60 letters" })
+  @IsDefined()
+  @IsString({ message: "" })
+  @IsNotEmpty({ message: "must not be empty" })
+  readonly password: string;
+}
