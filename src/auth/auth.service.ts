5d0346de7bd1eaad0f4f06feea2840e5b22ee0d7
Ivan Draganov <draganov.ivan.an@gmail.com>
2023-06-16 11:33:49 +0200

    try

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -38,46 +38,10 @@ export class AuthService {
     return user;
   }
 
-  // async login(user: any) {
-  //   return {
-  //     access_token: this.jwtService.sign(user._doc),
-  //   };
-  // }
-
-  // async signIn(username: string, pass: string): Promise<any> {
-  //   const user = await this.usersService.findOne(username);
-  //   if (user?.password !== pass) {
-  //     throw new UnauthorizedException({
-  //       message: `Incorrect password or email1 + ${username} ${pass}`,
-  //     });
-  //   }
-  //   const { password, ...result } = user;
-  //   // TODO: Generate a JWT and return it here
-  //   // instead of the user object
-  //   return result;
-  // }
-
   async login(username: string, pass: string): Promise<any> {
     const user = await this.validateUser(username, pass);
     return this.generateToken(user);
   }
-
-  // private async validateUser(username: string, pass: string) {
-  //   const user = await this.usersService.findOne(username);
-  //   if (!user) {
-  //     throw new UnauthorizedException({
-  //       message: `Incorrect password or email1 + ${username},pass: ${pass}`,
-  //     });
-  //   }
-  //   const passwordEquals = await bcrypt.compare(pass, user.password);
-
-  //   if (user && passwordEquals) {
-  //     return user;
-  //   }
-  //   throw new UnauthorizedException({
-  //     message: `Incorrect password or email2 + ${user} PASS:${user.password} EQUAL: ${passwordEquals}`,
-  //   });
-  // }
 
   async registration(dto: CreateUserDto) {
     const regUser = await this.usersService.findOne(dto.username);
@@ -85,13 +49,13 @@ export class AuthService {
       throw new HttpException("User already exists", HttpStatus.BAD_REQUEST);
     }
     const hashPassword = await bcrypt.hash(dto.password, 5);
-    if (hashPassword) {
-      throw new UnauthorizedException(
-        `Password: ${dto.password}, 
-        HASH: ${hashPassword},
-        DTO: ${dto}`
-      );
-    }
+    // if (hashPassword) {
+    //   throw new UnauthorizedException(
+    //     `Password: ${dto.password},
+    //     HASH: ${hashPassword},
+    //     DTO: ${dto}`
+    //   );
+    // }
     const user = await this.usersService.create({
       ...dto,
       password: hashPassword,
